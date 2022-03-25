8fb83e275107e855daaa06ef4f571c80f2ea7346
Geraldini2000 <geraldinimatos@gmail.com>
2022-03-25 16:57:18 -0300

    Finalizando cabecalho e rodape do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-<Navbar /> 
+  <Navbar :logo="logo_src" :alt="app_name" />  <!-- Chamada da imagem -->
   <router-view/>
-<Footer />
+  <Footer />
 </template>
 
 <script>
@@ -13,8 +13,33 @@ import Footer from './components/Footer.vue'
     components: {
       Navbar,
       Footer
+    },
+    data() {
+      return{ 
+      logo_src: "/img/logo.png", // caminho da imagem 
+      app_name: "Make Your Burger"
+      }
     }
   }
 </script>
 
+<style> /* estilo global */
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
+</style>
